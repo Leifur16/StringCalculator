11a6c295e79a3646ec5715e6f3c434590777a78d
Leifur16 <leifur16@ru.is>
2017-10-11 12:12:58 +0000

    part 1, testing empty string failure

---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,18 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	@Test
+	public void testEmpyString() {
+		assertEquals(0, Calculator.add(""));
+	}
+}
+
+// clean: $ rm -r classes/*
+// compile: $ javac src/main/java/is/ru/stringcalculator/*.java -d classes
+// compile test: javac -classpath "src/test/resources/junit-4.11.jar;classes/" src/test/java/is/ru/stringcalculator/CalculatorTest.java -d classes
+// run: $ java -cp "classes/;src/test/resources/junit-4.11.jar;src/test/resources/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore is.ru.stringcalculator.CalculatorTest
+ 
